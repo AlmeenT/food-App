99fc0130aee2f90372893e331214ea58e445b9a2
AlmeenT <tiamiyualameen@ymail.com>
2022-09-18 22:52:11 -0700

    header almost done

---
+++ lib/Widgets/big_texts.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+class BigTexts extends StatelessWidget {
+   Color? color;
+  final String text;
+  double size;
+  TextOverflow overFlow;
+BigTexts({ Key? key, this.color = const Color(0xFF332d2b),  required this.text, this.overFlow= TextOverflow.ellipsis, this.size=20 }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context){
+    return Text(
+      text,
+      overflow: overFlow,
+      style: TextStyle(
+        fontFamily: 'Roboto',
+        color: color,
+        fontSize: size,
+        fontWeight: FontWeight.w400
+      )
+    );
+  }
+}
--- lib/home/main_food_page.dart
+++ lib/home/main_food_page.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:food_app/Widgets/big_texts.dart';
+import 'package:food_app/utils/colors.dart';
 
 class MainFoodPage extends StatefulWidget {
   const MainFoodPage({ Key? key }) : super(key: key);
@@ -10,27 +12,42 @@ class MainFoodPage extends StatefulWidget {
 class _MainFoodPageState extends State<MainFoodPage> {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Container(
-        child: Row(
-          children: <Widget>[
-            Column(
-              children: <Widget>[
-                Text('Country'),
-                Text('City')
-              ],
-            ),
-            Container(
-              width: 45,
-              height: 45,
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(15),
-                color:  Colors.blue,
+    return Scaffold(
+      body: Column(
+        children: <Widget>[
+          Container(
+            child: Container(
+              margin: EdgeInsets.only(top:45, bottom:15),
+              padding: EdgeInsets.only(left:20, right:20),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: <Widget>[
+                  Column(
+                    children: <Widget>[
+                      BigTexts(text: 'Nigeria', color: AppColors.mainColor, ),
+                      Text('City')
+                    ],
+                  ),
+                  Center(
+                    child: Container(
+                      width: 45,
+                      height: 45,
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(15),
+                        color:  AppColors.mainColor
+                      ),
+                      child: Icon(
+                        Icons.search,
+                        color: Colors.white
+                      )
+                    ),
+                  )
+                ],
               )
             )
-          ],
-        )
-      )
+          ),
+      ]
+      ),
     );
   }
 }
